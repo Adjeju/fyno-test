9d59b0d670be4ecf5e2e1e1357dcf0ee8c934717
Andrii Miliuchenko <andrii2003@gmail.com>
2024-06-06 19:03:33 +0300

    fix

--- modules/home/views/gallery.tsx
+++ modules/home/views/gallery.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Grip } from "lucide-react";
@@ -18,29 +16,30 @@ export const Galery = ({ mainImageUrl, images }: Props) => (
     >
       <Grip className="mr-2 h-6 w-6" /> All photos
     </Button>
-    {/* <div className="relative h-[inherit] w-full"></div> */}
-    <img
-      src={mainImageUrl}
-      alt="main-image"
-      className="h-[inherit] w-full rounded-l-3xl object-cover"
-      // fill
-      // priority
-    />
-    <div className="grid h-[inherit] grid-cols-2 grid-rows-2 gap-1">
+    <div className="relative h-[inherit] w-full">
+      <Image
+        src={mainImageUrl}
+        alt="main-image"
+        className="rounded-l-3xl object-cover"
+        fill
+        priority
+      />
+    </div>
+    <div className="relative grid h-[inherit] grid-cols-2 grid-rows-2 gap-1">
       {images.map((url, idx) => (
-        <img
-          src={url}
-          alt="addition-image"
-          className={cn(
-            "h-full w-full object-cover",
-            idx === 1 && "rounded-tr-3xl",
-            idx === 3 && "rounded-br-3xl",
-          )}
-          // fill
-          // priority
-        />
-        // <div key={url} className="relative h-full w-full">
-        // </div>
+        <div key={url} className="relative h-full w-full">
+          <Image
+            src={url}
+            alt="addition-image"
+            className={cn(
+              "object-cover",
+              idx === 1 && "rounded-tr-3xl",
+              idx === 3 && "rounded-br-3xl",
+            )}
+            fill
+            priority
+          />
+        </div>
       ))}
     </div>
   </div>
